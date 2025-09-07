fd7b0c35ec5f17351d6f181603abe029732c1623
joeyRBH <joey@joeyholub.com>
2025-09-07 12:35:29 -0600

    Create rbhpractice.js

---
+++ styles/components/rbhpractice.js
@@ -0,0 +1,11 @@
+// Copy the ENTIRE React component code from the artifact here
+// This is the complete component I created for you
+import React, { useState } from 'react';
+import { Calendar, FileText, MessageCircle, User, LogOut, Plus, DollarSign, Users, Home, Check, X } from 'lucide-react';
+
+const RBHPractice = () => {
+  // [ALL THE CODE from the artifact goes here - the complete component]
+  // I'll provide this in the next step
+};
+
+export default RBHPractice;
