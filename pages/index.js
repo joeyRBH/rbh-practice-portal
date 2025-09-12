c6a1f1885612729f8ed50bae43dd211fb1949ad1
joeyRBH <joey@joeyholub.com>
2025-09-12 15:18:49 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
-import MindCareEHRPortal from '../components/MindCareEHRPortal'
+import dynamic from 'next/dynamic'
+
+const MindCareEHRPortal = dynamic(() => import('../components/MindCareEHRPortal'), {
+  ssr: false,
+  loading: () => <div style={{ 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '100vh',
+    fontSize: '1.5rem' 
+  }}>Loading MindCare Portal...</div>
+})
 
 export default function Home() {
   return <MindCareEHRPortal />
